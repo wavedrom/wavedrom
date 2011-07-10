1a92963043338039b7eaee6b18b53fc54bc4b7b8
alex.drom@gmail.com <alex.drom@gmail.com@a767234a-31a9-74d1-c766-0ce7ea12625b>
2011-07-10 04:04:04 +0000

    SVG export solution. fixes Issue 8
    git-svn-id: https://wavedrom.googlecode.com/svn/trunk@49 a767234a-31a9-74d1-c766-0ce7ea12625b

--- wavedrom.js
+++ wavedrom.js
@@ -1,6 +1,6 @@
 /*jslint white: true, onevar: true, undef: true, newcap: true, nomen: true, regexp: true, plusplus: true, bitwise: true, browser: true, strict: true, evil: true, maxerr: 500, indent: 4 */
 var WAVEDROM = {
-	version: "0.5.1",
+	version: "0.5.2",
 	lane: {},
 	canvas: {},
 	panela: {},
@@ -141,38 +141,133 @@ WAVEDROM.RenderGaps = function (root, source) {
 	}
 };
 
-WAVEDROM.SaveSVG = function (label) {
-	"use strict";
-	var w, f, d, name;
+WAVEDROM.utf8_encode = function (argString) {
+    // http://kevin.vanzonneveld.net
+    // +   original by: Webtoolkit.info (http://www.webtoolkit.info/)
+    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +   improved by: sowberry
+    // +    tweaked by: Jack
+    // +   bugfixed by: Onno Marsman
+    // +   improved by: Yves Sucaet
+    // +   bugfixed by: Onno Marsman
+    // +   bugfixed by: Ulrich
+    // +   bugfixed by: Rafal Kukawski
+    // *     example 1: utf8_encode('Kevin van Zonneveld');
+    // *     returns 1: 'Kevin van Zonneveld'
 
-	f = document.getElementById (label),
-	w = window.frames.w;
-	if (!w) {
-		w = document.createElement ('iframe');
-		w.id = 'w';
-		w.style.display = 'none';
-		document.body.insertBefore (w, null);
-		w = window.frames.w;
-		if (!w) {
-			w = window.open ('', '_temp', 'width=500, height=300');
-			if (!w) {
-				window.alert( 'Sorry, the file could not be created.' );
-				return false;
-			}
-		}
-	}
-	d = w.document;
-	name = "foo.svg";
-	d.open ('text/plain', 'replace');
-	d.close();
-	d.body.textContent = new XMLSerializer().serializeToString(f);
-	if (d.execCommand ('SaveAs', true, name)) {
-		window.alert (name + ' has been saved.');
-	} else {
-		window.alert ( 'The file has not been saved.\nIs there a problem?');
-	}
-	w.close();
-	return false;  //  don't submit the form
+    if (argString === null || typeof argString === "undefined") {
+        return "";
+    }
+
+    var string = (argString + ''); // .replace(/\r\n/g, "\n").replace(/\r/g, "\n");
+    var utftext = "",
+        start, end, stringl = 0;
+
+    start = end = 0;
+    stringl = string.length;
+    for (var n = 0; n < stringl; n++) {
+        var c1 = string.charCodeAt(n);
+        var enc = null;
+
+        if (c1 < 128) {
+            end++;
+        } else if (c1 > 127 && c1 < 2048) {
+            enc = String.fromCharCode((c1 >> 6) | 192) + String.fromCharCode((c1 & 63) | 128);
+        } else {
+            enc = String.fromCharCode((c1 >> 12) | 224) + String.fromCharCode(((c1 >> 6) & 63) | 128) + String.fromCharCode((c1 & 63) | 128);
+        }
+        if (enc !== null) {
+            if (end > start) {
+                utftext += string.slice(start, end);
+            }
+            utftext += enc;
+            start = end = n + 1;
+        }
+    }
+
+    if (end > start) {
+        utftext += string.slice(start, stringl);
+    }
+
+    return utftext;
+};
+
+WAVEDROM.base64_encode = function (data) {
+    // http://kevin.vanzonneveld.net
+    // +   original by: Tyler Akins (http://rumkin.com)
+    // +   improved by: Bayron Guevara
+    // +   improved by: Thunder.m
+    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +   bugfixed by: Pellentesque Malesuada
+    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // -    depends on: utf8_encode
+    // *     example 1: base64_encode('Kevin van Zonneveld');
+    // *     returns 1: 'S2V2aW4gdmFuIFpvbm5ldmVsZA=='
+    // mozilla has this native
+    // - but breaks in 2.0.0.12!
+    //if (typeof this.window['atob'] == 'function') {
+    //    return atob(data);
+    //}
+    var b64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+    var o1, o2, o3, h1, h2, h3, h4, bits, i = 0,
+        ac = 0,
+        enc = "",
+        tmp_arr = [];
+
+    if (!data) {
+        return data;
+    }
+
+    data = this.utf8_encode(data + '');
+
+    do { // pack three octets into four hexets
+        o1 = data.charCodeAt(i++);
+        o2 = data.charCodeAt(i++);
+        o3 = data.charCodeAt(i++);
+
+        bits = o1 << 16 | o2 << 8 | o3;
+
+        h1 = bits >> 18 & 0x3f;
+        h2 = bits >> 12 & 0x3f;
+        h3 = bits >> 6 & 0x3f;
+        h4 = bits & 0x3f;
+
+        // use hexets to index into b64, and append result to encoded string
+        tmp_arr[ac++] = b64.charAt(h1) + b64.charAt(h2) + b64.charAt(h3) + b64.charAt(h4);
+    } while (i < data.length);
+
+    enc = tmp_arr.join('');
+
+    switch (data.length % 3) {
+    case 1:
+        enc = enc.slice(0, -2) + '==';
+        break;
+    case 2:
+        enc = enc.slice(0, -1) + '=';
+        break;
+    }
+
+    return enc;
+};
+
+WAVEDROM.ViewSVG = function (label) {
+	"use strict";
+	var f, ser, str;
+
+	f   = document.getElementById (label);
+	ser = new XMLSerializer();
+	str = '<!-- Created with WaveDrom -->\n' + ser.serializeToString (f);
+	window.open ('data:image/svg+xml;base64,' + this.base64_encode(str), '_blank');
+};
+
+WAVEDROM.ViewSourceSVG = function (label) {
+	"use strict";
+	var f, ser, str;
+
+	f   = document.getElementById (label);
+	ser = new XMLSerializer();
+	str = '<!-- Created with WaveDrom -->\n' + ser.serializeToString (f);
+	window.open ('view-source:data:image/svg+xml;base64,' + this.base64_encode(str), '_blank');
 };
 
 WAVEDROM.parseWaveLanes = function (source) {
@@ -239,7 +334,7 @@ WAVEDROM.FindLaneMarkers = function (lanetext) {
 
 WAVEDROM.RenderWaveLane = function (root, content) {
 	"use strict";
-	var i, j, k, g, gg, title, b, lanetext, labeltext, labels = [1],
+	var i, j, k, g, gg, title, b, lanetext, labeltext, labels = [1], nxt_xgmax,
 	xmax    = 0,
 	xgmax   = 0,
 	svgns   = 'http://www.w3.org/2000/svg',
@@ -260,10 +355,9 @@ WAVEDROM.RenderWaveLane = function (root, content) {
 			title.setAttribute ("text-anchor", "end");
 			title.appendChild (lanetext);
 			g.insertBefore (title, g.firstChild);
-			
-			if (title.getBBox().width > xgmax) {
-				xgmax = title.getBBox().width;
-			}
+
+			nxt_xgmax = Math.round(title.getBBox().width);
+			if (nxt_xgmax > xgmax) { xgmax = nxt_xgmax; }
 
 			gg = document.createElementNS(svgns, 'g');
 			gg.id = "wavelane_draw_" + j;
