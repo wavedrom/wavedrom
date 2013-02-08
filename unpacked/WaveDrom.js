485e8b81baa9789aaf57ef2a830c1ceeeeefc533
alex.drom@gmail.com <alex.drom@gmail.com@a767234a-31a9-74d1-c766-0ce7ea12625b>
2013-02-08 17:34:34 +0000

    start using native browser base64 encoder function (supported in Gecko/WebKit/Presto)
    git-svn-id: https://wavedrom.googlecode.com/svn/trunk@138 a767234a-31a9-74d1-c766-0ce7ea12625b

--- unpacked/WaveDrom.js
+++ unpacked/WaveDrom.js
@@ -1,111 +1,5 @@
 /*jslint white: true, onevar: true, undef: true, newcap: true, nomen: true, regexp: true, plusplus: true, bitwise: true, browser: true, strict: true, evil: true, maxerr: 500, indent: 4 */
 
-function utf8_encode (argString) {
-    // http://kevin.vanzonneveld.net
-    // +   original by: Webtoolkit.info (http://www.webtoolkit.info/)
-    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-    // +   improved by: sowberry
-    // +    tweaked by: Jack
-    // +   bugfixed by: Onno Marsman
-    // +   improved by: Yves Sucaet
-    // +   bugfixed by: Onno Marsman
-    // +   bugfixed by: Ulrich
-    // +   bugfixed by: Rafal Kukawski
-    // *     example 1: utf8_encode('Kevin van Zonneveld');
-    // *     returns 1: 'Kevin van Zonneveld'
-
-    if (argString === null || typeof argString === "undefined") {
-        return "";
-    }
-
-    var string = (argString + ''); // .replace(/\r\n/g, "\n").replace(/\r/g, "\n");
-    var utftext = "",
-        start, end, stringl = 0;
-
-    start = end = 0;
-    stringl = string.length;
-    for (var n = 0; n < stringl; n++) {
-        var c1 = string.charCodeAt(n);
-        var enc = null;
-
-        if (c1 < 128) {
-            end++;
-        } else if (c1 > 127 && c1 < 2048) {
-            enc = String.fromCharCode((c1 >> 6) | 192) + String.fromCharCode((c1 & 63) | 128);
-        } else {
-            enc = String.fromCharCode((c1 >> 12) | 224) + String.fromCharCode(((c1 >> 6) & 63) | 128) + String.fromCharCode((c1 & 63) | 128);
-        }
-        if (enc !== null) {
-            if (end > start) {
-                utftext += string.slice(start, end);
-            }
-            utftext += enc;
-            start = end = n + 1;
-        }
-    }
-
-    if (end > start) {
-        utftext += string.slice(start, stringl);
-    }
-
-    return utftext;
-}
-
-function base64_encode (data) {
-/*
-    // http://kevin.vanzonneveld.net
-    // +   original by: Tyler Akins (http://rumkin.com)
-    // +   improved by: Bayron Guevara
-    // +   improved by: Thunder.m
-    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-    // +   bugfixed by: Pellentesque Malesuada
-    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-    // +   improved by: Rafal Kukawski (http://kukawski.pl)
-    // -    depends on: utf8_encode
-    // *     example 1: base64_encode('Kevin van Zonneveld');
-    // *     returns 1: 'S2V2aW4gdmFuIFpvbm5ldmVsZA=='
-    // mozilla has this native
-    // - but breaks in 2.0.0.12!
-    //if (typeof this.window['atob'] == 'function') {
-    //    return atob(data);
-    //}
-*/
-    var b64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
-    var o1, o2, o3, h1, h2, h3, h4, bits, i = 0,
-        ac = 0,
-        enc = "",
-        tmp_arr = [];
-
-    if (!data) {
-        return data;
-    }
-
-    data = this.utf8_encode(data + '');
-
-    do { // pack three octets into four hexets
-        o1 = data.charCodeAt(i++);
-        o2 = data.charCodeAt(i++);
-        o3 = data.charCodeAt(i++);
-
-        bits = o1 << 16 | o2 << 8 | o3;
-
-        h1 = bits >> 18 & 0x3f;
-        h2 = bits >> 12 & 0x3f;
-        h3 = bits >> 6 & 0x3f;
-        h4 = bits & 0x3f;
-
-        // use hexets to index into b64, and append result to encoded string
-        tmp_arr[ac++] = b64.charAt(h1) + b64.charAt(h2) + b64.charAt(h3) + b64.charAt(h4);
-    } while (i < data.length);
-
-    enc = tmp_arr.join('');
-    
-    var r = data.length % 3;
-    
-    return (r ? enc.slice(0, r - 3) : enc) + '==='.slice(r || 3);
-
-}
-
 var JsonML; if ("undefined" === typeof JsonML) { JsonML = {}; }
 
 (function() {
@@ -396,7 +290,7 @@ var JsonML; if ("undefined" === typeof JsonML) { JsonML = {}; }
 })();
 
 var WaveDrom = {
-	version: "13.02.05",
+	version: "13.02.07",
 	timer: 0,
 	lane: {
 		xs     : 20,    // tmpgraphlane0.width
@@ -534,7 +428,7 @@ WaveDrom.ViewSVG = function (label) {
 	'<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">\n' +
 	'<!-- Created with WaveDrom -->\n' +
 	ser.serializeToString (f);
-	window.open ('data:image/svg+xml;base64,' + base64_encode(str), '_blank');
+	window.open ('data:image/svg+xml;base64,' + window.btoa (str), '_blank');
 };
 
 WaveDrom.ViewSourceSVG = function (label) {
@@ -547,7 +441,7 @@ WaveDrom.ViewSourceSVG = function (label) {
 	'<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">\n' +
 	'<!-- Created with WaveDrom -->\n' +
 	ser.serializeToString (f);
-	window.open ('view-source:data:image/svg+xml;base64,' + base64_encode(str), '_blank');
+	window.open ('view-source:data:image/svg+xml;base64,' + window.btoa (str), '_blank');
 };
 
 WaveDrom.parseWaveLanes = function (sig) {
