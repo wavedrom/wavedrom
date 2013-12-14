3af25771156fc6508139ee48e1d7c476c482ef2f
alex.drom@gmail.com <alex.drom@gmail.com@a767234a-31a9-74d1-c766-0ce7ea12625b>
2013-12-14 08:02:51 +0000

    added schematic drawing capability (simple tree)
    git-svn-id: https://wavedrom.googlecode.com/svn/trunk@236 a767234a-31a9-74d1-c766-0ce7ea12625b

--- unpacked/WaveDrom.js
+++ unpacked/WaveDrom.js
@@ -76,9 +76,9 @@ if (undefined === JsonML) { JsonML = {}; }
 						}
 					} else if (name === "class") {
 						elem.className = value;
-// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+						// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 						elem.setAttribute(name, value);
-// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+						// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 					} else if (EVTS[name]) {
 						addHandler(elem, name, value);
 
@@ -264,12 +264,12 @@ if (undefined === JsonML) { JsonML = {}; }
 				// in IE styles are effective immediately
 				return null;
 			}
-//!!!!!!!!!!!!!!
+			//!!!!!!!!!!!!!!
 			svgns   = 'http://www.w3.org/2000/svg';
 			var elem;
-//			elem = patch(document.createElement(tagName), jml, filter);
+			//			elem = patch(document.createElement(tagName), jml, filter);
 			elem = patch(document.createElementNS(svgns, tagName), jml, filter);
-//!!!!!!!!!!!!!!
+			//!!!!!!!!!!!!!!
 			// trim extraneous whitespace
 			trimWhitespace(elem);
 			return (elem && "function" === typeof filter) ? filter(elem) : elem;
@@ -287,7 +287,6 @@ if (undefined === JsonML) { JsonML = {}; }
 	/*bool*/ JsonML.isElement = function (/*JsonML*/ jml) {
 		return (jml instanceof Array) && ("string" === typeof jml[0]);
 	};
-
 })();
 
 var WaveDrom = {
@@ -297,7 +296,7 @@ var WaveDrom = {
 		xs     : 20,    // tmpgraphlane0.width
 		ys     : 20,    // tmpgraphlane0.height
 		xg     : 120,   // tmpgraphlane0.x
-//		yg     : 0,     // head gap
+		//		yg     : 0,     // head gap
 		yh0    : 0,     // head gap title
 		yh1    : 0,     // head gap 
 		yf0    : 0,     // foot gap
@@ -468,6 +467,7 @@ var WaveDrom = {
 	}
 };
 
+/*
 WaveDrom.ViewSVG = function (label) {
 	"use strict";
 	var f, ser, str;
@@ -493,6 +493,7 @@ WaveDrom.ViewSourceSVG = function (label) {
 	ser.serializeToString(f);
 	window.open('view-source:data:image/svg+xml;base64,' + window.btoa(str), '_blank');
 };
+*/
 
 WaveDrom.parseWaveLanes = function (sig) {
 	"use strict";
@@ -568,7 +569,7 @@ WaveDrom.RenderWaveLane = function (root, content, index) {
 					x: this.lane.tgo,
 					y: this.lane.ym,
 					class: 'info',
-//					fill: '#0041c4', // Pantone 288C
+				// fill: '#0041c4', // Pantone 288C
 					'text-anchor': 'end'
 				},
 				name // + '') // name
@@ -595,9 +596,9 @@ WaveDrom.RenderWaveLane = function (root, content, index) {
 			if (content[j][1]) {
 				for (i = 0; i < content[j][1].length; i += 1) {
 					b    = document.createElementNS(svgns, "use");
-//					b.id = "use_" + i + "_" + j + "_" + index;
+					// b.id = "use_" + i + "_" + j + "_" + index;
 					b.setAttributeNS(xlinkns, 'xlink:href', '#' + content[j][1][i]);
-//					b.setAttribute('transform', 'translate(' + (i * this.lane.xs) + ')');
+					// b.setAttribute('transform', 'translate(' + (i * this.lane.xs) + ')');
 					b.setAttribute('transform', 'translate(' + (i * this.lane.xs) + ')');
 					gg.insertBefore(b, null);
 				}
@@ -704,7 +705,7 @@ WaveDrom.RenderMarks = function (root, content, index) {
 					x: i * dx + x,
 					y: y,
 					'text-anchor': 'middle',
-//					fill: '#AAA'
+					//					fill: '#AAA'
 					class: 'muted'
 				}, tmp
 			]);
@@ -812,7 +813,7 @@ WaveDrom.RenderGaps = function (root, source, index) {
 				while (Stack.length) {
 					if (Stack.shift() === '|') {
 						b    = document.createElementNS(svgns, "use");
-//						b.id = "guse_" + pos + "_" + i + "_" + index;
+						//						b.id = "guse_" + pos + "_" + i + "_" + index;
 						b.setAttributeNS(xlinkns, 'xlink:href', '#gap');
 						b.setAttribute('transform', 'translate(' + (this.lane.xs * ((2 * pos + 1) * this.lane.period * this.lane.hscale - this.lane.phase)) + ')');
 						g.insertBefore(b, null);
@@ -1125,6 +1126,152 @@ WaveDrom.InsertSVGTemplate = function (index, parent, source) {
 	parent.insertBefore(node, null);
 };
 
+WaveDrom.InsertSVGTemplateAssign = function (index, parent, source) {
+	"use strict";
+	var node, e;
+	// cleanup
+	while (parent.childNodes.length) {
+		parent.removeChild(parent.childNodes[0]);
+	}
+	e = [
+		"svg",
+		{
+			id: "svgcontent_" + index,
+			xmlns:"http://www.w3.org/2000/svg",
+			"xmlns:xlink":"http://www.w3.org/1999/xlink",
+			height:512,
+			viewBox: "0 0 512 512",
+			width:512,
+			overflow:"hidden"
+		},
+		['style', '.pinname{font-size:12px; font-style:normal; font-variant:normal; font-weight:500; font-stretch:normal; text-align:center; text-anchor:end; font-family:Helvetica}']
+	];
+	node = JsonML.parse(e);
+	parent.insertBefore(node, null);
+};
+
+WaveDrom.RenderAssign = function (index, source) {
+	function render (tree, state) {
+		var head, y, i, ilen;
+		state.xmax = Math.max(state.xmax, state.x);
+		head = tree[0];
+		y = state.y;
+		ilen = tree.length;
+		for (i = 1; i < ilen; i++) {
+			if (Object.prototype.toString.call(tree[i]) === '[object Array]') {
+				state = render (tree[i], {x:(state.x+1), y:state.y, xmax:state.xmax});
+			} else {
+				tree[i] = {name:tree[i], x:state.x+1, y:state.y};
+				state.y += 2;
+			}
+		}
+		tree[0] = {name:tree[0], x:state.x, y:Math.round((y + (state.y-2))/2)};
+		state.x--;
+		return state;
+	};
+	function draw_body (type, inputs) {
+		var e, gates = {
+			not:  'm -16,0 4,0 m 0,-6 0,12 10,-6 z m 10,6 c 0,1.104569 0.895431,2 2,2 1.104569,0 2,-0.895431 2,-2 0,-1.104569 -0.895431,-2 -2,-2 -1.104569,0 -2,0.895431 -2,2 z',
+			buf:  'm -2,0 2,0 m -12,-6 0,12 10,-6 z m -4,6 4,0',
+			and:  'm -16,-10 5,0 c 6,0 11,4 11,10 0,6 -5,10 -11,10 l -5,0 z',
+			nand: 'm -16,-10 3,0 c 6,0 11,4 11,10 0,6 -5,10 -11,10 l -3,0 z M 2,0 C 2,1.104569 1.104569,2 0,2 -1.104569,2 -2,1.104569 -2,0 c 0,-1.104569 0.895431,-2 2,-2 1.104569,0 2,0.895431 2,2 z',
+			or:   'm -18,-10 4,0 c 6,0 12,5 14,10 -2,5 -8,10 -14,10 l -4,0 c 2.5,-5 2.5,-15 0,-20 z',
+			nor:  'M 2,0 C 2,1.10457 1.104569,2 0,2 -1.104569,2 -2,0.745356 -2,0 c 0,-0.745356 0.895431,-2 2,-2 1.104569,0 2,0.89543 2,2 z m -20,-10 2,0 c 6,0 12,5 14,10 -2,5 -8,10 -14,10 l -2,0 c 2.5,-5 2.5,-15 0,-20 z',
+			xor:  'm -21,-10 c 1,3 2,6 2,10 m 0,0 c 0,4 -1,7 -2,10 m 3,-20 4,0 c 6,0 12,5 14,10 -2,5 -8,10 -14,10 l -4,0 c 1,-3 2,-6 2,-10 0,-4 -1,-7 -2,-10 z',
+			nxor: 'm -21,-10 c 1,3 2,6 2,10 m 0,0 c 0,4 -1,7 -2,10 M 2,0 C 2,1.10457 1.104569,2 0,2 -1.104569,2 -2,0.745356 -2,0 c 0,-0.745356 0.895431,-2 2,-2 1.104569,0 2,0.89543 2,2 z m -20,-10 2,0 c 6,0 12,5 14,10 -2,5 -8,10 -14,10 l -2,0 c 1,-3 2,-6 2,-10 0,-4 -1,-7 -2,-10 z',
+			add:  'm -8,5 0,-10 m -5,5 10,0 m 3,0 c 0,4.418278 -3.581722,8 -8,8 -4.418278,0 -8,-3.581722 -8,-8 0,-4.418278 3.581722,-8 8,-8 4.418278,0 8,3.581722 8,8 z',
+			mul:  'm -4,4 -8,-8 m 0,8 8,-8 m 4,4 c 0,4.418278 -3.581722,8 -8,8 -4.418278,0 -8,-3.581722 -8,-8 0,-4.418278 3.581722,-8 8,-8 4.418278,0 8,3.581722 8,8 z',
+			box:  'm -16,-10 16,0 0,20 -16,0 z'
+		};
+		e = gates[type] || gates.box;
+		return ['path', {d: e, style: "color:#000;fill:#ffc;fill-opacity:1;stroke:#000;stroke-width:1;stroke-opacity:1"}];
+	};
+	function draw_gate (spec) { // ['type', [x,y], [x,y] ... ]
+		var i, ilen, ret = ['g'], ys = [];
+		ilen = spec.length;
+		for (i = 2; i < ilen; i++) {
+			ys.push(spec[i][1]);
+		};
+		ret.push(
+			['g',
+				{transform:"translate(16,0)"},
+				['path', {
+					d: 'M  '+spec[2][0]+','+Math.min.apply(null, ys)+' '+spec[2][0]+','+Math.max.apply(null, ys),
+					style:'fill:none;stroke:#000;stroke-width:1;stroke-opacity:1'
+				}]
+			]
+		);
+		for (i = 2; i < ilen; i++) {
+			ret.push(
+				['g',
+					['path',
+						{
+							d:'m  '+spec[i][0]+','+spec[i][1]+' 16,0',
+							style: "fill:none;stroke:#085;stroke-width:1;stroke-opacity:1"
+						}
+					]
+				]
+			);
+		};
+		ret.push(
+			['g',
+				{
+					transform:'translate('+spec[1][0]+','+spec[1][1]+')'
+				},
+				['title', spec[0]],
+				draw_body(spec[0], ilen-2)
+			]
+		);
+		return ret;
+	};
+	function draw_boxes (tree, xmax) {
+		var ret = ['g'], i, ilen, fx, fy, fname, spec = [];
+		if (Object.prototype.toString.call(tree) === '[object Array]') {
+			ilen = tree.length;
+			spec.push(tree[0].name);
+			spec.push([32*(xmax-tree[0].x), 8*tree[0].y]);
+			for (i = 1; i < ilen; i++) {
+				if (Object.prototype.toString.call(tree[i]) === '[object Array]') {
+					spec.push([32*(xmax-tree[i][0].x), 8*tree[i][0].y]);
+				} else {
+					spec.push([32*(xmax-tree[i].x), 8*tree[i].y]);
+				}
+			}
+			ret.push(draw_gate (spec));
+			for (i = 1; i < ilen; i++) {
+				ret.push(draw_boxes (tree[i], xmax));
+			}		
+		} else {
+			fname = tree.name;
+			fx = 32*(xmax-tree.x);
+			fy = 8*tree.y;
+			ret.push(
+				['g',
+					{transform: 'translate('+fx+','+fy+')'},
+					['title', fname],
+					['path', {d:'M 2,0 a 2,2 0 1 1 -4,0 2,2 0 1 1 4,0 z'}],
+					['text',
+						['tspan', {
+							x:"-4", y:"4",
+							class:"pinname"},
+							fname
+						]
+					]
+				]
+			);
+		}
+		return ret;
+	};
+
+	var tree, xmax, svg;
+	tree = source.assign;
+	xmax = render (tree, {x:0,y:0,xmax:0}).xmax;
+	//console.log (JSON.stringify(tree));
+	svg = draw_boxes (tree, xmax);
+	//console.log (JSON.stringify(svg));
+	document.getElementById("svgcontent_" + index).insertBefore(JsonML.parse(['g', {transform:"translate(128.5, 32.5)"}, svg]), null);
+};
+
 WaveDrom.RenderWaveForm = function (index) {
 	"use strict";
 	function erra (e) {
@@ -1143,12 +1290,17 @@ WaveDrom.RenderWaveForm = function (index) {
 		if (Object.prototype.toString.call(source) !== '[object Object]') {
 			return erra ({ message: "[Semantic]: The root has to be an Object: '{signal:[...]}'"});
 		};
-		if (!source.signal) {
-			return erra ({ message: "[Semantic]: 'signal:[...]' property is missing inside the root Object"});
+		if (source.signal) {
+			if (Object.prototype.toString.call(source.signal) !== '[object Array]') {
+				return erra ({ message: "[Semantic]: 'signal' object has to be an Array 'signal:[]'"});
+			}
+		} else if (source.assign) {
+			if (Object.prototype.toString.call(source.assign) !== '[object Array]') {
+				return erra ({ message: "[Semantic]: 'assign' object hasto be an Array 'assign:[]'"});
+			}
+		} else {
+			return erra ({ message: "[Semantic]: 'signal:[...]' or 'assign:[...]' property is missing inside the root Object"});
 		};
-		if (Object.prototype.toString.call(source.signal) !== '[object Array]') {
-			return erra ({ message: "[Semantic]: 'signal' object has to be an Array 'signal:[]'"});
-		}
 		return source;
 	};
 
@@ -1158,49 +1310,39 @@ WaveDrom.RenderWaveForm = function (index) {
 
 	source = eva (index);
 
-	WaveDrom.InsertSVGTemplate(index, document.getElementById('WaveDrom_Display_' + index), source);
-
-	this.parseConfig (source);
-
-	ret = this.rec(source.signal, {'x':0, 'y':0, 'xmax':0, 'width':[], 'lanes':[], 'groups':[]});
-
-	root          = document.getElementById("lanes_" + index);
-	groups        = document.getElementById("groups_" + index);
-
-	content  = this.parseWaveLanes(ret.lanes);
-	glengths = this.RenderWaveLane(root, content, index);
-	for (i in glengths) {
-		xmax = Math.max(xmax, (glengths[i] + ret.width[i]));
-	}
-	this.RenderMarks(root, content, index);
-	this.RenderArcs(root, ret.lanes, index, source);
-	this.RenderGaps(root, ret.lanes, index);
-	this.RenderGroups(groups, ret.groups, index);
-	this.lane.xg = Math.ceil((xmax - this.lane.tgo) / this.lane.xs) * this.lane.xs;
-
-	width  = (this.lane.xg + (this.lane.xs * (this.lane.xmax + 1)));
-	height = (content.length * this.lane.yo +
+	if (source.signal) {
+		WaveDrom.InsertSVGTemplate(index, document.getElementById('WaveDrom_Display_' + index), source);
+		this.parseConfig (source);
+		ret = this.rec(source.signal, {'x':0, 'y':0, 'xmax':0, 'width':[], 'lanes':[], 'groups':[]});
+		root          = document.getElementById("lanes_" + index);
+		groups        = document.getElementById("groups_" + index);
+		content  = this.parseWaveLanes(ret.lanes);
+		glengths = this.RenderWaveLane(root, content, index);
+		for (i in glengths) {
+			xmax = Math.max(xmax, (glengths[i] + ret.width[i]));
+		}
+		this.RenderMarks(root, content, index);
+		this.RenderArcs(root, ret.lanes, index, source);
+		this.RenderGaps(root, ret.lanes, index);
+		this.RenderGroups(groups, ret.groups, index);
+		this.lane.xg = Math.ceil((xmax - this.lane.tgo) / this.lane.xs) * this.lane.xs;
+		width  = (this.lane.xg + (this.lane.xs * (this.lane.xmax + 1)));
+		height = (content.length * this.lane.yo +
 		this.lane.yh0 + this.lane.yh1 + this.lane.yf0 + this.lane.yf1);
 
-//	if (this.lane.scale === 3) {
-//		uwidth  = '100%';
-//		uwidth  = (window.innerWidth - 15);
-//		uheight = '100%';
-//		uheight = (window.innerHeight - (10+7+16+7+(WaveDrom.panela.ys)+7+16+7+16+7));
-//	} else {
-//		uwidth  = this.lane.scale * width;
-//		uheight = this.lane.scale * height;
-//	}
-	// ???
-	uwidth  = width;
-	uheight = height;
+		uwidth  = width;
+		uheight = height;
 
-	svgcontent = document.getElementById("svgcontent_" + index);
-	svgcontent.setAttribute('viewBox', "0 0 " + width + " " + height);
-	svgcontent.setAttribute('width', uwidth);
-	svgcontent.setAttribute('height', uheight);
-	svgcontent.setAttribute('overflow', 'hidden');
-	root.setAttribute('transform', 'translate(' + (this.lane.xg + 0.5) + ', ' + ((this.lane.yh0 + this.lane.yh1) + 0.5) + ')');
+		svgcontent = document.getElementById("svgcontent_" + index);
+		svgcontent.setAttribute('viewBox', "0 0 " + width + " " + height);
+		svgcontent.setAttribute('width', uwidth);
+		svgcontent.setAttribute('height', uheight);
+		svgcontent.setAttribute('overflow', 'hidden');
+		root.setAttribute('transform', 'translate(' + (this.lane.xg + 0.5) + ', ' + ((this.lane.yh0 + this.lane.yh1) + 0.5) + ')');
+	} else if (source.assign) {
+		WaveDrom.InsertSVGTemplateAssign(index, document.getElementById('WaveDrom_Display_' + index), source);
+		WaveDrom.RenderAssign(index, source);
+	}
 };
 
 WaveDrom.ProcessAll = function () {
@@ -1216,10 +1358,10 @@ WaveDrom.ProcessAll = function () {
 			points.item(i).setAttribute('id', 'InputJSON_' + index);
 
 			node0 = document.createElement('div');
-//			node0.className += "WaveDrom_Display_" + index;
+			//			node0.className += "WaveDrom_Display_" + index;
 			node0.id = "WaveDrom_Display_" + index;
 			points.item(i).parentNode.insertBefore(node0, points.item(i));
-//			WaveDrom.InsertSVGTemplate(i, node0);
+			//			WaveDrom.InsertSVGTemplate(i, node0);
 			index += 1;
 		}
 	}
