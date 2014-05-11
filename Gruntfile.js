2db69969ae4fc8a58ae1d3b12330f0bfe52e8b16
drom <alex.drom@gmail.com>
2014-05-11 00:41:01 -0700

    split source code in to parts

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,21 +6,28 @@ module.exports = function(grunt) {
     jshint: {
       all: ['Gruntfile.js', 'src/*.js']
     },
+    concat: {
+      dist: {
+        src: ['src/JsonML.js', 'src/WaveDrom.js'],
+        dest: 'build/build.js',
+      },
+    },
     uglify: {
       options: {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
       build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
+        src: 'build/build.js',
+        dest: 'build/WaveDrom.js'
       }
     }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
 
   // Default task(s).
-  grunt.registerTask('default', ['jshint', 'uglify']);
+  grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
 };
