f67136ecdb43ca5707c753da2b53e9246f31fca8
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2015-03-13 23:49:59 -0700

    switch to eslint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,38 +1,38 @@
 module.exports = function(grunt) {
-  'use strict';
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    jshint: {
-      all: ['Gruntfile.js', 'src/*.js']
-    },
-    concat: {
-      dist: {
-        src: ['src/JsonML.js', 'src/WaveDrom.js', 'src/Save.js'],
-        dest: 'wavedrom.max.js'
-      }
-    },
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'wavedrom.max.js',
-        dest: 'WaveDrom.js'
-      }
-    },
-    clean: {
-      build: ['build'],
-      node: ['node_modules']
-    }
-  });
+    'use strict';
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        eslint: {
+            all: ['Gruntfile.js', 'src/WaveDrom.js', 'src/Save.js']
+        },
+        concat: {
+            dist: {
+                src: ['src/JsonML.js', 'src/WaveDrom.js', 'src/Save.js'],
+                dest: 'wavedrom.max.js'
+            }
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'wavedrom.max.js',
+                dest: 'WaveDrom.js'
+            }
+        },
+        clean: {
+            build: ['build'],
+            node: ['node_modules']
+        }
+    });
 
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-clean');
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-eslint');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-clean');
 
-  // Default task(s).
-  grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
+    // Default task(s).
+    grunt.registerTask('default', ['eslint', 'concat', 'uglify']);
 };
