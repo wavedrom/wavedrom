7a56371bbc2e444977a735300b8ede98fa0077ef
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2016-01-30 09:49:20 -0800

    quick fix for the function names

--- wavedrom.max.js
+++ wavedrom.max.js
@@ -1992,9 +1992,14 @@ module.exports = {
 },{}],31:[function(require,module,exports){
 'use strict';
 
+window.WaveDrom = window.WaveDrom || {};
+
 var index = require('./');
 
-window.WaveDrom = index;
+window.WaveDrom.ProcessAll = index.processAll;
+window.WaveDrom.RenderWaveForm = index.renderWaveForm;
+window.WaveDrom.EditorRefresh = index.editorRefresh;
+window.WaveDrom.eva = index.eva;
 
 /* eslint-env browser */
 
