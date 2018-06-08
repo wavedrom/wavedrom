5fd2c0ed441bb053804e5229547854ab80dc7f7b
1138-4EB <1138-4EB@users.noreply.github.com>
2018-06-08 17:05:46 +0100

    export skins and add vue.js example component

---
+++ vue/WaveDrom.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <textarea v-model="text" placeholder="JSON (WaveDrom)"></textarea>
+    <br>
+    <div :id="'a' +  id"></div>
+  </div>
+</template>
+
+<script>
+window.WaveSkin = require('wavedrom/skins/default.js');
+var WaveDrom = require('wavedrom');
+
+export default {
+  name: 'WaveDrom',
+  props: {
+    id: Number,
+    parentText: String
+  },
+  data () {
+    return {
+      text: this.parentText
+    }
+  },
+  watch: {
+    text: function() {
+      WaveDrom.renderWaveForm( this.id, JSON.parse(this.text), 'a' );
+    }
+  }
+}
+</script>
+
+<style>
+</style>
